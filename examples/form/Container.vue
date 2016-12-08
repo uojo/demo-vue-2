c2a24c8fadf9e8622aa6386a0062b273df4404d4
tango <uojo@tangodeMacBook-Pro.local>
2016-12-08 23:42:23 +0800

    save

---
+++ examples/form/Container.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+<p>{{a0}}</p>
+<div class="column is-12">
+    <label class="label" for="email">Email</label>
+    <p :class="{ 'control': true }">
+        <input v-validate data-vv-rules="required|email" :class="{'input': true, 'is-danger': errors.has('email') }" name="email" type="text" placeholder="Email">
+        <span v-show="errors.has('email')" class="help is-danger">{{ errors.first('email') }}</span>
+    </p>
+</div>
+</div>
+</template>
+
+<script>
+
+export default {
+	data:()=>{
+		// console.log("data",this); // undefined
+		return {
+			a0:"Hello",
+			a5:1,
+			a6:10,
+			a7:{
+				a:10,
+				b:2
+			}
+		}
+	},
+	props:["a1"],
+	methods:{
+		fn1(){
+			++this.a7.a;
+			++this.a7.b;
+		}
+	}
+}
+</script>
+
---
+++ examples/form/index.js
@@ -0,0 +1,12 @@
+// import 'babel-polyfill' //会使打包之后的文件增大！
+import Vue from 'vue'
+import Container from './Container.vue'
+import VeeValidate from 'vee-validate';
+
+Vue.use(VeeValidate);
+
+// 创建根组件
+var demo = new Vue({
+	el: '#app',
+	render:h=> h( Container ),
+});
